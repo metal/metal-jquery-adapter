50905d363b054fe160e3839374521f50456e39ef
Maira Bello <maira.araujo@liferay.com>
2015-06-03 14:13:05 -0300

    Uses metal-tasks and renames project with new organization

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,8 @@
 'use strict';
 
-var esformatter = require('gulp-esformatter');
-var gulp = require('gulp');
-var jshint = require('gulp-jshint');
+var metalTasks = require('metal-tasks');
 
-require('metaljs')({
-  bundleFileName: 'jquery-adapter.js'
-});
-
-var codeFileGlobs = ['src/**/*.js', 'test/**/*.js', 'gulpfile.js'];
-gulp.task('format', function() {
-  var gulpOpts = {
-    base: process.cwd()
-  };
-  return gulp.src(codeFileGlobs, gulpOpts)
-    .pipe(esformatter())
-    .pipe(gulp.dest(process.cwd()));
-});
-
-gulp.task('lint', function() {
-  return gulp.src(codeFileGlobs)
-    .pipe(jshint())
-    .pipe(jshint.reporter(require('jshint-stylish')));
+metalTasks({
+  bundleFileName: 'jquery-adapter.js',
+  skipCssBuild: true
 });
