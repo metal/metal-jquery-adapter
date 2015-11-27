b44b8a31b014c7101ea34c7a43682071e57de6f1
Maira Bello <maira.araujo@liferay.com>
2015-11-27 13:12:44 -0300

    Updates version of gulp-metal to 0.7.0

--- karma.conf.js
+++ karma.conf.js
@@ -1,29 +1,6 @@
-var metal = require('gulp-metal');
-
-var babelOptions = {
-  resolveModuleSource: metal.renameAlias,
-  sourceMap: 'both'
-};
+var metalKarmaConfig = require('metal-karma-config');
 
 module.exports = function (config) {
-  config.set({
-    frameworks: ['mocha', 'chai', 'sinon', 'source-map-support', 'commonjs'],
-
-    files: [
-      'bower_components/jquery/dist/jquery.js',
-      'bower_components/metal/**/*.js',
-      'src/**/*.js',
-      'test/**/*.js'
-    ],
-
-    preprocessors: {
-      'src/**/*.js': ['babel', 'commonjs'],
-      'bower_components/metal/**/*.js': ['babel', 'commonjs'],
-      'test/**/*.js': ['babel', 'commonjs']
-    },
-
-    browsers: ['Chrome'],
-
-    babelPreprocessor: {options: babelOptions}
-  });
-};
+	metalKarmaConfig(config);
+	config.files.push('bower_components/jquery/dist/jquery.js');
+}
